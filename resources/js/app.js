6126f0a3f853b24880ee55da89932bc79d140650
ehsan <ehsansepahi277@gmail.com>
2023-04-29 04:12:50 +0430

    add realtime online notification

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,14 @@
 import './bootstrap';
+
+Echo.channel('notification')
+    .listen('UserSessionChanged', (e) => {
+        const notificationElement = document.getElementById('notification');
+
+        notificationElement.innerText = e.message;
+        notificationElement.classList.remove('invisible');
+        notificationElement.classList.remove('alert-success');
+        notificationElement.classList.remove('alert-danger');
+
+        notificationElement.classList.add('alert-' + e.type);
+
+    });
